275cab797be67b003b53719ebfbf663ef5c0b4ed
João Victor  Costa <jbezerra30@hotmail.com>
2025-08-22 17:18:51 +0000

    parte novato em xadrez

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,35 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    int bispo, torre, rainha;
+    
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    for (bispo = 0; bispo <= 5; bispo++) {
+                 
+        printf("♗ se movimentou na diagonal ➚: %d\n", bispo);
+    }
+    printf ("================================================\n");
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
+    
+    for (torre = 0; torre <=7; torre++){
+
+     printf ("♖ se movimentou em linha reta ⬆: %d\n",torre);
+    }
+
+
+
+    printf ("================================================\n");
+
+    
+    for (rainha = 0; rainha <=5; rainha++){
+        printf ("♕ se movimentou verticalmente:%d\n",rainha);
+    }
+    
+       return 0;
+
 }
